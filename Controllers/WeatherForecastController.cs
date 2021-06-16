d46beeb529d25028048b20ef35ca3bda1d8f3ba0
Andrea Tosato <andrea.tosato@hotmail.it>
2021-06-16 23:14:12 +0200

    Create sample

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+﻿using Dapr;
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace DaprMqttCS.Controllers
+{
+    public class Driver
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    [ApiController]
+    [Route("[Controller]")]
+    public class DriverDetailsController : ControllerBase
+    {
+        private readonly DaprClient client;
+
+        public DriverDetailsController(DaprClient client)
+        {
+            this.client = client;
+        }
+
+        [Topic("mqtttest", "driverdetails")]
+        [HttpPost("driverdetails")]
+        public async Task<IActionResult> ProcessMessage(Driver driver)
+        {
+            Console.WriteLine($"----------> {driver.FirstName} {driver.LastName}");
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("Sender")]
+        public async Task<IActionResult> Sender([FromBody] Driver driver)
+        {
+            await client.PublishEventAsync("mqtttest", "driverdetails", driver);
+            return Created("driverdetails", driver);
+        }
+
+    }
+}
